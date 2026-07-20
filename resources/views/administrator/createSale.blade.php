fac499bf1e82a07ea0285e46888e21ec7af95c29
taufiq <htaufiq225@gmail.com>
2026-07-20 09:33:34 +0800

    feat: add create sale view and integrate flowbite into app entry point

--- resources/views/administrator/createSale.blade.php
+++ resources/views/administrator/createSale.blade.php
@@ -263,7 +263,7 @@
                     </svg>
                     Simpan Penjualan
                 </button>
-                <a href="/sales" class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none cursor-pointer">Batal</a>
+                <a href="{{ route('sales.index') }}" class="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none cursor-pointer">Batal</a>
             </div>
         </form>
     </div>
